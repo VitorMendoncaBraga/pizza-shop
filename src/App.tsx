44f63de7b6fb7a53804151c5265b0755d4db3e9d
Vitor Mendonça Braga <vmbbraga5@gmail.com>
2025-07-26 17:38:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+export function App() {
+  
+
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <Button>
+          Enviar
+      </Button>
+    </div>
+  )
+}
+
